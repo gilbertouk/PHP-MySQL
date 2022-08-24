07151cf7dd16be31cff3754b3338018aca53b0d6
Gilberto Antonio <103536077+gilbertouk@users.noreply.github.com>
2022-08-24 12:15:34 +0100

    Segurança e Hash

--- includes/login.php
+++ includes/login.php
@@ -1,3 +1,17 @@
 <?php
+    function gerarHash($senha){
+        $hash = password_hash($senha, PASSWORD_DEFAULT);
+        return $hash;
+    }
 
+    function testarHash($senha, $hash){
+        $ok = password_verify($senha, $hash);
+        return $ok;
+    }
+
+    if (testarHash('123456', '$2y$10$T/TPMRZr1k2/lG.lih3MnODIIJnh0gQ6IAQsSkPyOo56r5.VY77YW')){
+        echo "Senha confere";
+    } else {
+        echo "Senha nao confere";
+    }
 ?>
